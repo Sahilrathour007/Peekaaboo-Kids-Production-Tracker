c0071553aacb21782380aa3d19f949ecb322ce94
Sahilrathour007 <rathoursahil109@gmail.com>
2026-08-14 12:21:11 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.PEEKAABOO_SUPABASE = {
+     url: "https://drctfjrjdtpprxeycfll.supabase.co",
+     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyY3RmanJqZHRwcHJ4ZXljZmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDI2MTksImV4cCI6MjEwMDExODYxOX0.7uC6HirVLActwhshJI5kp57Ju12vUdVrSlM1rgShzSY"
+   };
